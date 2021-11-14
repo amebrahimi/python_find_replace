e5c98ed2d90605cbf43c2c101994bda07fa10919
amebrahimi <amirebrahimi5@live.com>
2021-11-14 22:26:38 +0330

    upgrade functions

--- main.py
+++ main.py
@@ -19,6 +19,16 @@ def replace_word(file, word, new_word):
         f.writelines(lines)
 
 
+# find word in file
+def find_word(file, word):
+    with open(file, "r") as f:
+        lines = f.readlines()
+        for i, line in enumerate(lines):
+            if word in line:
+                return True
+    return False
+
+
 def read_from_json_file_and_convert_to_python_object(file_name):
     with open(file_name, "r", encoding="utf-8") as f:
         return json.load(f)
@@ -26,30 +36,40 @@ def read_from_json_file_and_convert_to_python_object(file_name):
 
 def convert_json_object_keys_to_snake_case(json_object):
     new_json_object = {}
-    old_object_keys = {}
+    old_object_keys_with_new_values = {}
     for key, value in json_object.items():
         new_key = snake_case(key)
         new_json_object[new_key] = value
-        old_object_keys[key] = new_key
-    return new_json_object, old_object_keys
+        old_object_keys_with_new_values[key] = new_key
+    return new_json_object, old_object_keys_with_new_values
 
 
 def snake_case(word):
-    new_word = ""
-    for i, letter in enumerate(word):
-        if letter.isupper() and i != 0:
-            new_word += "_" + letter.lower()
+    snake_case_string = ""
+    for i in range(len(word)):
+        if i == 0:
+            snake_case_string += word[i].lower()
+        elif word[i].isupper() and word[i - 1].islower():
+            snake_case_string += "_" + word[i].lower()
+        elif word[i] == ' ' and word[i - 1].islower():
+            snake_case_string += "_"
         else:
-            new_word += letter.lower()
-    return new_word
+            snake_case_string += word[i].lower()
+    return snake_case_string
 
 
 def write_json_object_to_disk(json_object, file_name):
     with open(file_name, "w", encoding="utf-8") as f:
         json.dump(json_object, f, ensure_ascii=False, indent=4)
 
+
 def sort_object_by_key(object):
     return dict(sorted(object.items()))
+
+
+def write_set_to_disk_as_json_array(set, file_name):
+    with open(file_name, "w", encoding="utf-8") as f:
+        json.dump(list(set), f, ensure_ascii=False, indent=4)
 
 
 if __name__ == "__main__":
@@ -64,8 +84,15 @@ if __name__ == "__main__":
 
     write_json_object_to_disk(sort_object_by_key(snake_case), "en.json")
 
-    # for file in files:
-    #     for key, val in old_object.items():
-    #         replace_word(
-    #             file, f"context.translate('{key}')", f"context.translate('{val}')"
-    #         )
+    key_not_used = set()
+    for key, _ in old_object.items():
+        key_not_used.add(key)
+
+    for file in files:
+        for key, val in old_object.items():
+            if find_word(file, f"'{key}'"):
+                if key in key_not_used:
+                    key_not_used.remove(key)
+                replace_word(file, f"'{key}'", f"'{val}'")
+
+    write_set_to_disk_as_json_array(key_not_used, "key_not_used.json")
